9b0f5c278a79d4b75c4595b7e489ad0237c2b9e2
cegbe <chi.egbe@yahoo.com>
2026-08-02 15:30:31 +0100

    oya over to github

---
+++ hey.py
@@ -0,0 +1,13 @@
+# print("\n\t - Python Basics \n \t - Data Structures \n \t - AI \n\n")
+
+# print("""\t - Python Basics
+#     \t - Data Structures
+#      \t - AI""")
+
+phone = "176-1234-34"
+print(phone.replace("-", ""))
+
+
+phone = "+49 (176) 123-4567"
+print(phone.replace("+", "").replace(" ",
+      "").replace("(", "").replace("-", "").replace(")", ""))
